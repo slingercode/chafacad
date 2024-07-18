1351167959d4204dcbce2b787541b3721a2bdd48
Noel Espino Córdova <github@ednoesco.com>
2024-07-17 20:28:19 -0600

    Use Raylib (#1)

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,12 +4,10 @@ set -xe
 
 mkdir -p bin
 
-FLAGS="-Wall -Wextra -pedantic"
-FRAMEWORKS="-framework Cocoa -framework OpenGL -framework IOKit"
-BIN="-o bin/chafacad"
 SRC="src/main.c"
-INCLUDE="-I /opt/homebrew/Cellar/glfw/3.4/include -I /opt/homebrew/Cellar/glew/2.2.0_1/include"
-LINKS="-L /opt/homebrew/Cellar/glfw/3.4/lib -L /opt/homebrew/Cellar/glew/2.2.0_1/lib"
-LIBS="-l glfw3 -l GLEW"
+BIN="-o bin/chafacad"
+FRAMEWORKS="-framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL"
+INCLUDES="-I include/"
+RAYLIB="lib/libraylib.a"
 
-clang $FLAGS $FRAMEWORKS $BIN $SRC $INCLUDE $LINKS $LIBS
+clang $SRC $BIN $FRAMEWORKS $INCLUDES $RAYLIB
--- src/main.c
+++ src/main.c
@@ -1,84 +1,19 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-
-#include <stdio.h>
+#include "raylib.h"
 
 #define CHAFACAD "ChafaCAD"
 
-void error_callback(int error, const char* description) {
-    fprintf(stderr, "Error [%d]: %s\n", error, description);
-}
-
-/*
- * [GLFW documentation - Window closing and close flag](https://www.glfw.org/docs/latest/window_guide.html#window_close)
- */
-void close_window_callback(GLFWwindow* window) {
-    glfwSetWindowShouldClose(window, GLFW_TRUE);
-}
-
-static void key_window_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-        close_window_callback(window);
-    }
-}
-
 int main(void) {
-    glfwSetErrorCallback(error_callback);
+    InitWindow(800, 450, CHAFACAD);
 
-    /* Initialize GLFW */
-    if (!glfwInit()) {
-        fprintf(stderr, "Error: There was an error trying to initialize GLFW\n");
+    while (!WindowShouldClose()) {
+        BeginDrawing();
 
-        return 1;
-    }
-    
-    /* MacOS only. Set the maximum OpenGL version available */
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        ClearBackground(BLACK);
 
-    /* Create a windowed mode window and its OpenGL context */
-    GLFWwindow* window = glfwCreateWindow(640, 480, CHAFACAD, NULL, NULL);
-    if (window == NULL) {
-        fprintf(stderr, "Error: Could not create a window\n");
-        glfwTerminate();
-
-        return 1;
+        EndDrawing();
     }
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    glewExperimental = GL_TRUE;
-
-    /* Initialize GLEW */
-    glewInit();
-
-    const GLubyte *renderer = glGetString(GL_RENDERER);
-    const GLubyte *version = glGetString(GL_VERSION);
-    printf("Renderer: %s\n", renderer);
-    printf("OpenGL version supported: %s\n", version);
-
-    /* Set close callback when the windows is going to be closed */
-    glfwSetWindowCloseCallback(window, close_window_callback);
-
-    /* Set the keys callback */
-    glfwSetKeyCallback(window, key_window_callback);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window)) {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
+    CloseWindow();
 
     return 0;
 }
